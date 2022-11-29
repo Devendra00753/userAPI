f7cedb209926ffa751a22b0e638eff546a1db6d2
Devendra00753 <119424758+Devendra00753@users.noreply.github.com>
2022-11-29 19:52:51 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-let express=require("express");
-let app=express();
-app.use(express(json());
-app.get('/',(req,res)=>{
-  console.log("getting data");
-})        
-app.listen(3000,()=>{
-  console.log("server running");
-})
